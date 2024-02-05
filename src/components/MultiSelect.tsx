c2073089eb79751647f2ff8ccbe3fce25f35a320
akincris <cristina@Cristinas-MacBook-Air.local>
2024-02-05 17:39:32 +0200

    dd env file and dropdown component

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -1,48 +1,92 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ChevronDownIcon, XMarkIcon } from "@heroicons/react/20/solid";
+import { ICharacter } from "../interfaces/character";
 
 export const MultiSelect = () => {
   const [value, setInput] = useState("");
-  const [enteredValues, setEnteredValues] = useState([
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [enteredValues, setEnteredValues] = useState<ICharacter[]>([]);
+
+  const values = [
     { name: "Rick Bestun", episodes: 22, imageURL: "" },
     { name: "Morty Mc.warren", episodes: 24, imageURL: "" },
-    { name: "Black", episodes: 5, imageURL: "" },
-  ]);
+    { name: "Mrs. Black", episodes: 5, imageURL: "" },
+  ]
 
-  return (
-    <div className="flex justify-between p-4 rounded-xl border border-slate-400 shadow-md bg-white">
-      <div className="flex flex-wrap gap-2">
-        {enteredValues.map((item, i) => (
-          <div
-            className="flex gap-2 bg-slate-200 px-2 py-1 rounded-lg text-slate-600"
-            key={i}
+  const MultiValue = ({ data }: { data: any }) => {
+    return (
+      <div className="flex gap-2 bg-slate-200 px-2 py-1 rounded-lg text-slate-600">
+        <div>{data.name}</div>
+        <div className="flex items-center">
+          <button
+            className="bg-slate-500 p-0.5 rounded"
+            onClick={() => {
+              const updatedValues = enteredValues.filter((value) => value.name !== data.name);
+              setEnteredValues(updatedValues);
+            }}
           >
-            <div>{item.name}</div>
-            <div className="flex items-center">
-              <button
-                onClick={() => {
-                  const updatedValues = enteredValues.filter(
-                    (value) => value.name !== item.name
-                  );
-                  setEnteredValues(updatedValues);
-                }}
-              >
-                <XMarkIcon className="h-4 w-4 m-1 bg-slate-500 rounded text-white" />
-              </button>
-            </div>
-          </div>
-        ))}
-        <div className="flex">
-          <input
-            type="text"
-            value={value}
-            onChange={(e) => setInput(e.target.value)}
-          />
+            <XMarkIcon className="h-4 w-4 text-white" />
+          </button>
         </div>
       </div>
-      <button>
-        <ChevronDownIcon className="w-6 h-6 fill-slate-500" />
-      </button>
+    );
+  };
+
+  const DropdownItem = ({ data }: { data: any }) => {
+    return (
+      <div className="p-2 px-3 flex gap-3 text-slate-600 items-center border-b border-slate-400">
+        <input
+          type="checkbox"
+          checked={!!enteredValues.find((value) => value.name === data.name)}
+          onChange={(value) => {
+            if (value.target.checked) {
+              setEnteredValues([...enteredValues, data]);
+            } else {
+              const updatedValues = enteredValues.filter((v) => v.name !== data.name);
+              setEnteredValues(updatedValues);
+            }
+          }}
+        />
+        <div className="flex gap-2 items-center">
+          <img className="rounded h-8 w-8" src={data.imageURL} />
+          <div className="flex flex-col">
+            <div>
+              <span className="font-semibold text-slate-700">{data.name}</span>
+            </div>
+            <span className="text-xs">{`${data.episodes} episodes`}</span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className="flex flex-col gap-4">
+      <div
+        className={`flex justify-between p-4 rounded-xl border border-slate-400 shadow-md bg-white`}
+        onClick={() => inputRef.current?.focus()}
+      >
+        <div className="flex flex-wrap gap-2">
+          {enteredValues.map((item, i) => (
+            <MultiValue key={i} data={item} />
+          ))}
+          <div className="flex">
+            <input
+              className="outline-0"
+              ref={inputRef}
+              value={value}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
+        </div>
+        <button>
+          <ChevronDownIcon className="w-6 h-6 fill-slate-500" />
+        </button>
+      </div>
+      <div className="max-h-[20rem] overflow-auto scroll-smooth bg-slate-50 border border-slate-400 rounded-xl">
+        {values.map((item, i) => (
+          <DropdownItem key={i} data={item} />
+        ))}
+      </div>
     </div>
   );
 };
